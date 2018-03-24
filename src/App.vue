ccd6a9181437bcf3690713f62b72d63bbcaabb0d
zxfjd3g <258147149@qq.com>
2018-03-24 15:53:33 +0800

    demo3

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,22 @@
 <template>
-  <div class="todo-container">
-    <div class="todo-wrap">
-     <!-- <TodoHeader @addTodo="addTodo"/>--><!--给TodoHeader标签对象绑定addTodo事件监听-->
-      <TodoHeader ref="header"/>
-      <TodoList :todos="todos"/>
-      <todo-footer>
-        <input type="checkbox" v-model="isAllCheck" slot="checkAll"/>
-        <span slot="count">已完成{{completeSize}} / 全部{{todos.length}}</span>
-        <button class="btn btn-danger" v-show="completeSize" @click="deleteCompleteTodos" slot="delete">清除已完成任务</button>
-      </todo-footer>
-    </div>
+  <div class="container">
+    <Search/>
+    <users-main/>
   </div>
 </template>
-<!--
-绑定事件监听  ----订阅消息
-触发事件  ----发布消息
 
-
-
--->
 <script>
-  import PubSub from 'pubsub-js'
-  import TodoHeader from './components/TodoHeader.vue'
-  import TodoList from './components/TodoList.vue'
-  import TodoFooter from './components/TodoFooter.vue'
-  import storageUtil from './util/storageUtil'
+  import Search from './components/Search.vue'
+  import Main from './components/Main.vue'
 
   export default {
-
-    data () {
-      return {
-        // 从localStorage读取todos
-        todos: storageUtil.readTodos()
-      }
-    },
-
-    computed: {
-      completeSize () {
-        return this.todos.reduce((preTotal, todo) => preTotal + (todo.complete?1:0), 0)
-      },
-
-      isAllCheck: {
-        get () {
-          return this.completeSize===this.todos.length && this.completeSize>0
-        },
-
-        set (value) {// value是当前checkbox最新的值
-          this.selectAllTodos(value)
-        }
-      }
-    },
-
-    mounted () {// 执行异步代码
-      // 给<TodoHeader/>绑定addTodo事件监听
-      //this.$on('addTodo', this.addTodo)  // 给App绑定的监听, 不对
-      this.$refs.header.$on('addTodo', this.addTodo)
-
-      // 订阅消息
-      PubSub.subscribe('deleteTodo', (msg, index) => {
-        this.deleteTodo(index)
-      })
-    },
-
-    methods: {
-      addTodo (todo) {
-        this.todos.unshift(todo)
-      },
-
-      deleteTodo (index) {
-        this.todos.splice(index, 1)
-      },
-
-      // 删除所有选中的todo
-      deleteCompleteTodos () {
-        this.todos = this.todos.filter(todo => !todo.complete)
-      },
-
-      // 全选/全不选
-      selectAllTodos (check) {
-        this.todos.forEach(todo =>todo.complete = check)
-      }
-    },
-
-    watch: { // 监视
-      todos: {
-        deep: true, //深度监视
-        /*handler: function (value) {
-          // 将todos最新的值的json数据, 保存到localStorage
-          //window.localStorage.setItem('todos_key', JSON.stringify(value))
-          storageUtil.saveTodos(value)
-        }*/
-        handler: storageUtil.saveTodos,
-        /*handler: function(todos) {
-          window.localStorage.setItem(TODOS_KEY, JSON.stringify(todos))
-        },*/
-      }
-    },
-
     components: {
-      TodoHeader,
-      TodoList,
-      TodoFooter
+      Search,
+      UsersMain: Main
     }
   }
 </script>
 
 <style>
-  .todo-container {
-    width: 600px;
-    margin: 0 auto;
-  }
-  .todo-container .todo-wrap {
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-  }
+
 </style>
